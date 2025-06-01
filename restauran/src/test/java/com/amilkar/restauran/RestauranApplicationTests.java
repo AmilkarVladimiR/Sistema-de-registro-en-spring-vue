d6cf58547285a71c21d356f5ac305485ed30a017
AmilkarVladimiR <reyesamilkarreyes@gmail.com>
2025-06-01 13:27:37 -0600

    Primer commit: subir proyecto inicial

---
+++ restauran/src/test/java/com/amilkar/restauran/RestauranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amilkar.restauran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
